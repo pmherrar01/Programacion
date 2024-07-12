b5921f85985a7447374b8191521c5d45f4926f43
pablo <pmherrerar01@educarex.es>
2024-07-12 15:02:37 +0200

    Ejercicio14 terminado

--- tema7ArraysDeObjetos/Ejercicio13/src/Ejercicio14.java
+++ tema7ArraysDeObjetos/Ejercicio13/src/Ejercicio14.java
@@ -55,6 +55,15 @@ public class Ejercicio14 {
         return ENTRADA.nextLine();
     }
 
+    public static int pedirPosicion(){
+        Scanner teclado = new Scanner(System.in);
+        int posicion;
+        System.out.print("Introduzca posicion: ");
+        posicion = teclado.nextInt();
+        posicion--;  //Tener en cuenta que para el usuario las posiciones NO empiezan en cero.
+        return posicion;
+    }
+
     public static Alumno rellenarDatosAlumno() {
         Alumno alumno;
         String nombre;
@@ -70,25 +79,89 @@ public class Ejercicio14 {
         return alumno;
     }
 
-    public static void rellenarAlumnos(Alumno[] vAlumnos) {
-        int i = 0;
-
-        for (Alumno alumno : vAlumnos) {
-            System.out.print("Alumno " + (i + 1));
-            alumno = rellenarDatosAlumno();
-            vAlumnos[i] = alumno;
-
-            i++;
-        }
+    public static void insertarUnAlumno(Alumno[] vAlumnos){
+        Alumno alumno;
+        int posicion;
+        boolean posicionVacia;
+        
+        do{
+            posicionVacia = true;
+            posicion = pedirPosicion();
+            try{
+                if  (vAlumnos[posicion] != null){
+                    posicionVacia = false;
+                    System.out.println("Celda ocupada por el alumno "+vAlumnos[posicion].getNombre());
+                }
+            }
+            catch(ArrayIndexOutOfBoundsException e){
+                posicionVacia = false;
+                System.out.println("La posición debe estar comprendida entre 1 y "+vAlumnos.length);
+            }
+        }while(!posicionVacia);  //es lo mismo que poner (posicionVacia == false)
+        
+        alumno = rellenarDatosAlumno();
+        vAlumnos[posicion] = alumno;
+        System.out.println("Alumno insertado correctamente.");
     }
 
     public static void mostrarAlumnos(Alumno[] vAlumnos) {
-        int i = 0;
-        for (Alumno alumno : vAlumnos) {
-            System.out.print("Alumno " + (i + 1));
-            System.out.println(alumno.toString());
+        int i;
 
-            i++;
+        for(i = 0; i < vAlumnos.length; i++){
+            if (vAlumnos[i] == null) {
+                System.out.print("\n");
+            } else {
+                System.out.println("Posicion "+ (i+1) + ": " + "\n" + vAlumnos[i]);
+            }
+        }
+    }
+
+    public static void mostrarAlumnoPorNota(Alumno[] vAlumnos){
+        int num;
+
+        num = pedirNum("Introduce el numero para mostrar los alumnos con nota media superior: ");
+
+        for(int i = 0; i < vAlumnos.length; i++){
+            if (vAlumnos[i] == null) {
+                System.out.print("\n");
+
+            }else{
+                if(vAlumnos[i].getNotaMedia() >= num){
+                    System.out.println(vAlumnos[i]);
+                }
+            }
+            
+        }
+    }
+
+    public static void mostrarAlumnoNotaMediaSuspensa(Alumno[] vAlumnos){
+        for(Alumno alumno : vAlumnos){
+            if (alumno == null) {
+                System.out.println("");
+            } else {
+                if(alumno.getNotaMedia() < 5){
+                    System.out.println(alumno);
+                }   
+            }
+        }
+    }
+
+    public static void buscarAlumno(Alumno[] vAlumnos){
+        String nombre;
+
+        nombre = pedirString("Alumno que quieres buscar:");
+
+        for(Alumno  alumno : vAlumnos){
+            if (alumno == null) {
+                System.out.println("");
+            } else {
+                if(alumno.getNombre().equalsIgnoreCase(nombre)){
+                    System.out.println("El alumno si esta matriculado.");
+                    System.out.println(alumno);
+                }else{
+                    System.out.println("El alumno no es matriculado.");
+                }   
+            }
         }
     }
 
@@ -106,19 +179,19 @@ public class Ejercicio14 {
 
             switch (num) {
                 case 1:
-                    rellenarAlumnos(vAlumnos);
+                    insertarUnAlumno(vAlumnos);
                     break;
                 case 2:
                     mostrarAlumnos(vAlumnos);
                     break;
                 case 3:
-
+                    mostrarAlumnoPorNota(vAlumnos);
                     break;
                 case 4:
-
+                    mostrarAlumnoNotaMediaSuspensa(vAlumnos);
                     break;
                 case 5:
-
+                    buscarAlumno(vAlumnos);
                     break;
                 case 6:
                     System.out.println("Saliendo...");
